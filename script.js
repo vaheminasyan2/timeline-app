535859a39102d8fe14980d4aa42ed85ae028be37
vaheminasyan2 <vaheminasyan2@gmail.com>
2020-02-20 15:16:27 -0800

    added status persistance using localStorage

--- script.js
+++ script.js
@@ -9,36 +9,68 @@ function toggleComplete() {
 toggleButton.onclick = toggleComplete;
 
 var div1 = document.getElementsByClassName("intro")[0];
-
+if (localStorage.getItem("intro") === "complete") {
+  div1.classList.toggle("complete");
+}
 $(".intro").click(function() {
   div1.classList.toggle("complete");
+  if (localStorage.getItem("intro") === "complete") {
+    localStorage.setItem("intro", " ");
+  } else {
+    localStorage.setItem("intro", "complete");
+  }
 });
 
 var div2 = document.getElementsByClassName("devnews")[0];
-
+if (localStorage.getItem("devnews") === "complete") {
+  div2.classList.toggle("complete");
+}
 $(".devnews").click(function() {
   div2.classList.toggle("complete");
+  if (localStorage.getItem("devnews") === "complete") {
+    localStorage.setItem("devnews", " ");
+  } else {
+    localStorage.setItem("devnews", "complete");
+  }
 });
 
-toggleButton.onclick = toggleComplete;
 var div3 = document.getElementsByClassName("topic1")[0];
-
+if (localStorage.getItem("topic1") === "complete") {
+  div3.classList.toggle("complete");
+}
 $(".topic1").click(function() {
   div3.classList.toggle("complete");
+  if (localStorage.getItem("topic1") === "complete") {
+    localStorage.setItem("topic1", " ");
+  } else {
+    localStorage.setItem("topic1", "complete");
+  }
 });
 
-toggleButton.onclick = toggleComplete;
 var div4 = document.getElementsByClassName("topic2")[0];
-
+if (localStorage.getItem("topic2") === "complete") {
+  div4.classList.toggle("complete");
+}
 $(".topic2").click(function() {
   div4.classList.toggle("complete");
+  if (localStorage.getItem("topic2") === "complete") {
+    localStorage.setItem("topic2", " ");
+  } else {
+    localStorage.setItem("topic2", "complete");
+  }
 });
 
-toggleButton.onclick = toggleComplete;
 var div5 = document.getElementsByClassName("wrapup")[0];
-
+if (localStorage.getItem("wrapup") === "complete") {
+  div5.classList.toggle("complete");
+}
 $(".wrapup").click(function() {
   div5.classList.toggle("complete");
+  if (localStorage.getItem("wrapup") === "complete") {
+    localStorage.setItem("wrapup", " ");
+  } else {
+    localStorage.setItem("wrapup", "complete");
+  }
 });
 
 $("#edit").click(function() {
